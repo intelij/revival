ea760cfce5e4ae6d5182458b5992314461d1b078
Khululekani Mkhonza <ks.mkhonza@gmail.com>
2021-07-17 00:38:27 +0100

    remove stars

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -119,7 +119,15 @@
 <script src="/default/js/validator.min.js"></script>
 <!-- Custom Js -->
 <script src="/default/js/main.js"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-55216857-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
+    gtag('config', 'UA-55216857-1');
+</script>
 </body>
 
 </html>
